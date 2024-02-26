6a0c64b51425fbdd8490c391ce9c5aacf6b6e6a3
Jmoore100 <jjm00033@mix.wvu.edu>
2024-02-26 00:08:09 -0500

    amplifyapp commit
    amplifyapp commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>CS 230</h1>
+        <h2>Section 001</h2>
+        <p>WVU ID: 800401095</p>
+        <P>Hi I am Joshua Moore</P>
+    </div>
+  );
+}
+
+export default App;
